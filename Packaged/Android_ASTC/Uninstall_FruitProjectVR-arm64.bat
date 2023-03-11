065d7280996cf066ca8c77e957826d877a1bde66
Ramon Zuniga <ramonzuniga9@gmail.com>
2023-03-11 13:50:17 -0600

    Fruit VR Demo for Oculus 2
    Made with Unreal 4

---
+++ Packaged/Android_ASTC/Uninstall_FruitProjectVR-arm64.bat
@@ -0,0 +1,19 @@
+setlocal
+if NOT "%UE_SDKS_ROOT%"=="" (call %UE_SDKS_ROOT%\HostWin64\Android\SetupEnvironmentVars.bat)
+set ANDROIDHOME=%ANDROID_HOME%
+if "%ANDROIDHOME%"=="" set ANDROIDHOME=C:/Users/Ramon/AppData/Local/Android/Sdk
+set ADB=%ANDROIDHOME%\platform-tools\adb.exe
+set DEVICE=
+if not "%1"=="" set DEVICE=-s %1
+for /f "delims=" %%A in ('%ADB% %DEVICE% shell "echo $EXTERNAL_STORAGE"') do @set STORAGE=%%A
+@echo.
+@echo Uninstalling existing application. Failures here can almost always be ignored.
+%ADB% %DEVICE% uninstall com.lazyhound.vrfruit
+@echo.
+echo Removing old data. Failures here are usually fine - indicating the files were not on the device.
+%ADB% %DEVICE% shell rm -r %STORAGE%/UE4Game/FruitProjectVR
+%ADB% %DEVICE% shell rm -r %STORAGE%/UE4Game/UE4CommandLine.txt
+%ADB% %DEVICE% shell rm -r %STORAGE%/obb/com.lazyhound.vrfruit
+%ADB% %DEVICE% shell rm -r %STORAGE%/Android/obb/com.lazyhound.vrfruit
+@echo.
+@echo Uninstall completed
